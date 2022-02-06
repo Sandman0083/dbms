ceea0557a598c1482649452daa2d570df588ce09
Sandman0083 <82945200+Sandman0083@users.noreply.github.com>
2022-02-07 02:02:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+const express = require("express");
+const app = express();
+const mySql = require("mysql");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+app.use(cors())
+app.use(express.json())
 
 
-ReactDOM.render(
+const db = mySql.createConnection({
+    database:"education",
+    host:"localhost",
+    user:"root",
+    password:""
+})
+
+
+
+app.post('/login', (req, res)=>{
+    dept= req.body.dept,
+   userId= req.body.userId,
+   password= req.body.password
+   db.query("SELECT * FROM admin WHERE userid=? AND password=? AND department=?",[userId, password, dept], (err, result)=>{
+   
+   if(result.length>0){
+       const token = jwt.sign(
+           {
+               user: result.userId,
+               dept: result.dept
+           }, 'secret123'
+       )
+       return res.json({status:'ok', user:token})
+   }
+   else{
+    res.send({message : "Wrong Username or Password", user:false})
+   }
+
+})
+})
+
+
+
+app.post('/add',(req,res)=>{
+    course_id	= req.body.subCode,
+    link = req.body.link,
+    sem_id = req.body.sem
+ db.query("INSERT INTO course (course_id, link, sem_id) VALUES(?,?,?)",[course_id, link, sem_id], (err, result)=>{
+     if (err) {
+         console.log(err);
+     } else{
+         console.log("success");
+     }
+ })
   
-    <App /> ,
-  
-  document.getElementById('root')
-);
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vital
+
+app.get('/getdata', (req,res)=>{
+    db.query("SELECT * FROM course", (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+           res.send(result)
+        }
+    })
+})
+
+
+app.listen(3001, ()=>{
+    console.log("serever is running");
+})
